6390433ec8e06629c677a6ee3f0b5db7be20ba5a
aditya lucky <adityalucky52@gmail.com>
2025-06-07 11:28:25 +0530

    second commit

--- social-media/src/App.jsx
+++ social-media/src/App.jsx
@@ -5,17 +5,23 @@ import Header from "./Components/Header";
 import PostList from "./Components/PostList";
 
 import SideBar from "./Components/SideBar";
+import React, { useState } from "react";
 
 function App() {
+   const [selectedTab, setSelectedTab] = useState("Home");
+
    return (
       <>
          <div className="app-container">
-            <SideBar />
+            <SideBar
+               selectedTab={selectedTab}
+               setSelectedTab={setSelectedTab}
+            />
 
             <div className="content">
                <Header />
-               <CreatePost></CreatePost>
-               <PostList></PostList>
+               {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+
                <Footer />
             </div>
          </div>
