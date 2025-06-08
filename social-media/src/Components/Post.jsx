79c75581a80f70001f0e61a5556f6ed3659259f9
aditya lucky <adityalucky52@gmail.com>
2025-06-08 10:23:43 +0530

    commit post

--- social-media/src/Components/Post.jsx
+++ social-media/src/Components/Post.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { AiFillDelete } from "react-icons/ai";
+import { useContext } from "react";
+import { PostList } from "../store/post-list-store";
 
 function Post({ post }) {
+   const { deletePost } = useContext(PostList);
    return (
       <div className="card post-card" style={{ width: "30rem" }}>
          <div className="card-body">
             <h5 className="card-title">
                {post.title}
 
-               <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+               <span
+                  className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger "
+                  pnclick={() => deletePost(post.id)}
+               >
                   <AiFillDelete />
                </span>
             </h5>
