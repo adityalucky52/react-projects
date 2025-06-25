fafedb3a75713ef30dfa66b5cef1839a63f12f7e
aditya lucky <adityalucky52@gmail.com>
2025-06-25 22:50:13 +0530

    world-atlas

--- world-atlas/src/Pages/Contact.jsx
+++ world-atlas/src/Pages/Contact.jsx
@@ -1,8 +1,54 @@
 
 
 function Contact() {
+
+  const handleonformsubmit = (formData) => {
+    // e.preventDefault();
+    
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+    // e.target.reset(); 
+  }
+
   return (
-    <h1>contact</h1>
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+    <div className="contact-wrapper container">
+       
+        <form action={handleonformsubmit}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="enter your name"
+            name="username"
+            required
+            autoComplete="off"
+          />
+
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter you email"
+            name="email"
+            required
+            autoComplete="off"
+          />
+
+          <textarea
+            className="form-control"
+            rows="10"
+            placeholder="Enter your message"
+            name="message"
+            required
+            autoComplete="off"
+          ></textarea>
+
+          <button type="submit" value="send">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
